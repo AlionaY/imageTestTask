2d9115e7aa1241c6957ff1eacb0deb542857a691
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-04-03 18:12:20 +0300

    Parse image dimension

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 import asyncio
 
 import aiohttp
 from PIL import ImageFile
 
 
-async def get_image_dimensions(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                content = await response.content.read(1024)
-                parser = ImageFile.Parser()
-                parser.feed(content)
-                if parser.image:
-                    return parser.image.size
-            else:
-                return None, None
+async def fetch_urls_from_sheet(sheet_name, batch_size):
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    gc = gspread.authorize(credentials)
+
+    sheet = gc.open(sheet_name).sheet1
+    urls = sheet.col_values(1)[1:]
+
+    batches = [urls[i:i + batch_size] for i in range(0, len(urls), batch_size)]
+    return batches
+
+
+async def get_image_dimensions(session, url):
+    async with session.get(url) as response:
+        if response.status == 200:
+            content = await response.content.read(1024)
+            parser = ImageFile.Parser()
+            parser.feed(content)
+            if parser.image:
+                return parser.image.size
+        else:
+            return None, None
+
+
+async def process_batch(session, batch):
+    tasks = []
+    for url in batch:
+        task = asyncio.create_task(get_image_dimensions(session, url))
+        tasks.append(task)
+    results = await asyncio.gather(*tasks)
+    return results
 
 
 async def main():
-    url = "https://data.sanitino.eu/PRODUCT-45548/f09ec09abcd7a97936ccbb0d?size=feed-1080"
-    width, height = await get_image_dimensions(url)
-    if width is not None and height is not None:
-        print(f"The dimensions of the image are {width}x{height} pixels")
-    else:
-        print("Failed to fetch image dimensions")
+    sheet_name = "Image Parser Test Task"
+    batch_size = 100
+
+    batches = await fetch_urls_from_sheet(sheet_name, batch_size)
+
+    async with aiohttp.ClientSession() as session:
+        for batch in batches:
+            results = await process_batch(session, batch)
+            print(results)
 
 
 if __name__ == "__main__":
